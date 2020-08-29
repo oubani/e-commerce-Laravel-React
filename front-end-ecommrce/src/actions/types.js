1b442073c7c79d44eaf4ccbfd583ad882a093665
unknown <oubaniayoub@gmail.com>
2020-08-29 21:52:47 +0100

    create cart files

--- front-end-ecommrce/src/actions/types.js
+++ front-end-ecommrce/src/actions/types.js
@@ -10,3 +10,8 @@ export const CHECK_LOGIN = 'CHECK_LOGIN';
 export const GETNEWPRODUCTS = 'NEWPRODUCTS';
 export const GETMOSTORDERED = 'MOSTORDERED';
 export const GETHIGHPROMOTION = 'HIGHPROMOTION';
+
+// Cart Types
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const EMPTY_CART = 'EMPTY_CART';
