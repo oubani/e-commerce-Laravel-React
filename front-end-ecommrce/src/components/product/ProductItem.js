94dd3bb08a9b8ffb76e7b34d119b35fe38258f5b
oubani <oubaniayoub@gmail.com>
2020-12-11 15:28:20 +0100

    handle add to favorite

--- front-end-ecommrce/src/components/product/ProductItem.js
+++ front-end-ecommrce/src/components/product/ProductItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { FaHeart, FaCartPlus, FaRegHeart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { addToCart } from '../../actions/cartAction';
 
 const ProductItem = ({ product, addToCart }) => {
   const { id, name, prix, thumbnail } = product;
+
+  if (localStorage.getItem('favorites') === null) {
+    let LocalFavorites = [];
+    localStorage.setItem('favorites', JSON.stringify(LocalFavorites));
+  }
+
+  const addToFavorite = (id) => {
+    if (localStorage.getItem('favorites') === null) {
+      let LocalFavorites = [];
+      localStorage.setItem('favorites', JSON.stringify(LocalFavorites));
+    }
+    var localFavorites = JSON.parse(localStorage.getItem('favorites'));
+    console.log(localFavorites.includes(id));
+    if (!localFavorites.includes(id)) {
+      localFavorites.push(id);
+      localStorage.setItem('favorites', JSON.stringify(localFavorites));
+    }
+  };
+
+  const favorites = JSON.parse(localStorage.getItem('favorites'));
 
   const addClick = ({ product }, qte = 1) => {
     const newProduct = {
@@ -16,21 +36,32 @@ const ProductItem = ({ product, addToCart }) => {
       quantity: qte,
     };
     addToCart(1, newProduct);
-    // console.log(newProduct);
   };
 
   return (
     <div className='productItem'>
-      <img src={`http://localhost:8000/images/${thumbnail}`} alt={name} className='imgProduct' />
-      <p className='proTitle'>{name}</p>
+      <Link style={{ color: 'black' }} to={`/product/${id}`}>
+        <img
+          src={`http://localhost:8000/images/${thumbnail}`}
+          alt={name}
+          className='imgProduct'
+        />
+        <p className='proTitle'>{name}</p>
+      </Link>
       <p> {prix} Dh</p>
       <div className='proPrice'>
         <p>
           <FaCartPlus className='clickedIcon' onClick={addClick} />
         </p>
         <p>
-          {/* <FaHeart className='clickedIcon' /> */}
-          <FaRegHeart className='clickedIcon' />
+          {!favorites.includes(id) ? (
+            <FaRegHeart
+              className='clickedIcon'
+              onClick={() => addToFavorite(id)}
+            />
+          ) : (
+            <FaHeart className='clickedIcon' />
+          )}
         </p>
       </div>
     </div>
