d491d91c2adf6b094a8f59f9b42de72a481fbdd4
oubani <oubaniayoub@gmail.com>
2021-01-09 16:31:30 +0100

    install chart js 2

--- front-end-ecommrce/src/components/charts/SoldChart.js
+++ front-end-ecommrce/src/components/charts/SoldChart.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 const SoldChart = () => {
+  const [dailyData, setDailyData] = useState([1, 2]);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            data: [12, 21, 15, 23, 11],
+            label: 'Solds ',
+            borderColor: '#1e3d59',
+            backgroundColor: 'rgba(0,0,0,0)',
+          },
+        ],
+      }}
+    />
+  ) : null;
+
   return (
     <div>
       <h1>Sold</h1>
-      <LinearScale />
+      {lineChart}
     </div>
   );
 };
--- front-end-ecommrce/src/components/charts/VisitsChart.js
+++ front-end-ecommrce/src/components/charts/VisitsChart.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
 
 const VisitsChart = () => {
+  const [dailyData, setDailyData] = useState([1, 2]);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: [1, 2, 3, 4, 5],
+        datasets: [
+          {
+            data: [120, 61, 105, 123, 211],
+            label: 'Solds $',
+            borderColor: '#ff6e40',
+            backgroundColor: 'rgba(0,0,0,0)',
+          },
+        ],
+      }}
+    />
+  ) : null;
+
   return (
     <div>
       <h1>Visits</h1>
+      {lineChart}
     </div>
   );
 };
